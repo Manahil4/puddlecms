cac92b2ea2bb68f0b15c775e47a47f06e5c6a5fc
Manahil Ejaz <manahilejaz16@gmail.com>
2024-07-01 00:47:24 -0700

    Initial commit

---
+++ puddle/urls.py
@@ -0,0 +1,28 @@
+# Import the admin module from Django's contrib package, which provides the admin interface.
+from django.contrib import admin
+
+# Import the path function from Django's urls module, used to define URL patterns.
+from django.urls import path
+
+# Import the index and contact view from the core.views module. This view will handle requests to the root URL.
+from core.views import index, contact
+
+
+# Define the URL patterns list. Each entry in the list is a URL pattern that maps a URL to a view.
+urlpatterns = [
+    
+    # This pattern routes requests to the root URL ('') to the index view.
+    # Example: http://www.example.com/
+    # 'name="index"' names this URL pattern as 'index', allowing it to be referred to by this name elsewhere in the project.
+    path('', index, name="index"),
+
+    
+    # This pattern routes requests with the 'admin/' URL to the Django admin interface.
+    # Example: http://www.example.com/admin/
+    path('admin/', admin.site.urls),
+    
+    # This pattern routes requests to the root URL + contact/ ('contact/') to the contact view.
+    # Example: http://www.example.com/contact/
+    # 'name="contact"' names this URL pattern as 'contact', allowing it to be referred to by this name elsewhere in the project.
+    path('contact/', contact, name = "contact")
+    ]
