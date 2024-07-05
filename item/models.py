91710d13044677f52672c96bf32095c92661d259
Manahil Ejaz <manahilejaz16@gmail.com>
2024-07-05 07:53:09 -0700

    error resolved

--- item/models.py
+++ item/models.py
@@ -11,13 +11,13 @@ class Category(models.Model):
         return self.name
     
 class Item(models.Model):
-    category = models.ForeignKey(Category, related_name='item',on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='items',on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
     is_sold = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, related_name='item', on_delete = models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='items', on_delete = models.CASCADE)
     image = models.ImageField(upload_to='item_images',blank=True,null=True)
     def __str__(self) :
         return self.name
