26f463eb7e47a6b0a845c8cc6864623805dcc910
Manahil Ejaz <manahilejaz16@gmail.com>
2024-07-05 23:33:25 -0700

    error resolved

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+from .forms import LoginForm
+app_name='core'
+urlpatterns = [
+    path("", views.index, name="index"),
+    # This pattern routes requests to the root URL + contact/ ('contact/') to the contact view.
+    # Example: http://www.example.com/contact/
+    # 'name="contact"' names this URL pattern as 'contact', allowing it to be referred to by this name elsewhere in the project.
+    path('contact/', views.contact, name = "contact"),
+    path('signup/', views.signup, name="signup"),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html',authentication_form=LoginForm), name='login'),
+]
+
+    
--- puddle/urls.py
+++ puddle/urls.py
@@ -6,27 +6,19 @@ from django.contrib import admin
 # Import the path function from Django's urls module, used to define URL patterns.
 from django.urls import path,include
 
-# Import the index and contact view from the core.views module. This view will handle requests to the root URL.
-from core.views import index, contact
-
-
 # Define the URL patterns list. Each entry in the list is a URL pattern that maps a URL to a view.
 urlpatterns = [
     
     # This pattern routes requests to the root URL ('') to the index view.
     # Example: http://www.example.com/
     # 'name="index"' names this URL pattern as 'index', allowing it to be referred to by this name elsewhere in the project.
-    path('', index, name="index"),
+    path('', include('core.urls')),
 
     
     # This pattern routes requests with the 'admin/' URL to the Django admin interface.
     # Example: http://www.example.com/admin/
     path('admin/', admin.site.urls),
     
-    # This pattern routes requests to the root URL + contact/ ('contact/') to the contact view.
-    # Example: http://www.example.com/contact/
-    # 'name="contact"' names this URL pattern as 'contact', allowing it to be referred to by this name elsewhere in the project.
-    path('contact/', contact, name = "contact"),
     path('myitems/', include("myitems.urls") ),
     path('item/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
