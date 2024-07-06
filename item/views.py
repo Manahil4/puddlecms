e587060cc57367dabf15e38023803377ef61e788
Manahil Ejaz <manahilejaz16@gmail.com>
2024-07-06 11:13:24 -0700

    edit and delete

--- item/views.py
+++ item/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Item
-from . forms import AddNewItemForm
+from . forms import AddNewItemForm, EditItemForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def detail(request,pk):
@@ -24,3 +24,26 @@ def new(request):
     return render(request, 'item/form.html',{
         'form':form,
         'title':'Add New item'})
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    
+    if request.method =='POST':
+        form = EditItemForm(request.POST,request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item:detail',pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request, 'item/form.html',{
+        'form':form,
+        'title':'Edit item'})
+     
+        
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+    
+    return redirect('Dashboard:index')
